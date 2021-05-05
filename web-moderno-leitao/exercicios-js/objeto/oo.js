1256652d8a4dda69caf055b09f6c9427636f31f6
Sergio Oseko <osk.sergio@gmail.com>
2021-05-05 07:11:08 -0300

    Início da aula 92 Introdução à OO.

---
+++ web-moderno-leitao/exercicios-js/objeto/oo.js
@@ -0,0 +1,21 @@
+// CÓDIGO NÃO EXECUTÁVEL! (APENAS CONCEITOS SOBRE ORIENTAÇÃO A OBJETOS)
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento()  // Na orientação a objetos, o foco passou as ser o objeto.
+
+// Princípios importantes na OO
+//      1. Abstração >> obter dados do mundo real para inserir no mundo do software
+//      2. Encapsulamento >> certas informações do mundo real não devem ficar expostas ao usuário 
