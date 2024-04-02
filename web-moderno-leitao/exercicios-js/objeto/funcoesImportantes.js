98ab4b14ae9e68a700502c0b18590c6c01bf21ed
Sergio Oseko <osk.sergio@gmail.com>
2024-04-02 06:21:14 -0300

    feat: fim da aula #120

--- web-moderno-leitao/exercicios-js/objeto/funcoesImportantes.js
+++ web-moderno-leitao/exercicios-js/objeto/funcoesImportantes.js
@@ -22,3 +22,13 @@ Object.defineProperty(people, 'dataNascimento', {
 people.dataNascimento = '01/03/2024'
 console.log(people.dataNascimento)
 console.log(Object.keys(people))
+
+// Object.assign (ECMAScript 2015)
+const dest = { a: 1 }
+const o1 = { b: 2 }
+const o2 = { c: 3, a: 4 }
+const obj = Object.assign(dest, o1, o2)
+
+Object.freeze(obj)
+obj.c = 1234
+console.log(obj)
